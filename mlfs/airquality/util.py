0ff2e6058ca37805575592618458750f5646de95
Pontus Lüthi <pontus.luthi@gmail.com>
2025-11-15 01:22:58 +0100

    did stuff

--- mlfs/airquality/util.py
+++ mlfs/airquality/util.py
@@ -121,7 +121,7 @@ def get_city_coordinates(city_name: str):
     Takes city name and returns its latitude and longitude (rounded to 2 digits after dot).
     """
     # Initialize Nominatim API (for getting lat and long of the city)
-    geolocator = Nominatim(user_agent="MyApp")
+    geolocator = Nominatim(user_agent="AirQualityPredictionApp")
     city = geolocator.geocode(city_name)
 
     latitude = round(city.latitude, 2)
